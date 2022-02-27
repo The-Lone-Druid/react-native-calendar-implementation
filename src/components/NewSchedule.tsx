a5bfd29c2927d9d5a027c23ee8e2b690cb90dc8f
Zahid Shaikh <Zahid.786shaikh@gmail.com>
2022-02-27 22:40:44 +0530

    Add Schedule work in progress

--- src/components/NewSchedule.tsx
+++ src/components/NewSchedule.tsx
@@ -1,9 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 import { Button, Colors, Modal, Portal, TextInput, Title } from 'react-native-paper'
 import { Formik } from 'formik';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { AntDesign } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const NewSchedule = ({ visible, hideModal }: any) => {
+    const [date, setDate] = useState(new Date(1598051730000));
+    const [mode, setMode]: any = useState('date');
+    const [show, setShow] = useState(false);
+    const [formDate, setFormDate] = useState('');
+    const [formTime, setTime] = useState('');
+
+    const onChange = (event: any, selectedDate: any) => {
+        setShow(Platform.OS === 'ios');
+        setDate(selectedDate);
+        setFormDate(selectedDate);
+        console.log(formDate);
+        console.log(event);
+    };
+
+    const showMode = (currentMode: any) => {
+        setShow(true);
+        setMode(currentMode);
+    };
+
+    const showDatepicker = () => {
+        showMode('date');
+    };
+
+    const showTimepicker = () => {
+        showMode('time');
+    };
+
     const containerStyle = { backgroundColor: 'white', padding: 20, borderRadius: 10, marginHorizontal: 20 };
 
     return (
@@ -22,11 +52,15 @@ const NewSchedule = ({ visible, hideModal }: any) => {
                 >
                     {(formikProps) => (
                         <View>
-                            <View style={{marginBottom: 20}}>
+                            <View style={{ marginBottom: 20 }}>
                                 <Title>Add a new Schedule</Title>
                             </View>
                             {/* Schedule title input */}
                             <TextInput
+                                style={{
+                                    marginTop: 10,
+                                    backgroundColor: 'white',
+                                }}
                                 label="Schedule Title"
                                 value={formikProps.values.scheduleTitle}
                                 placeholder="Schedule Title"
@@ -36,7 +70,10 @@ const NewSchedule = ({ visible, hideModal }: any) => {
                             />
                             {/* Schedule Description input */}
                             <TextInput
-                                style={{marginTop: 10}}
+                                style={{
+                                    marginTop: 10,
+                                    backgroundColor: 'white',
+                                }}
                                 label="Description"
                                 value={formikProps.values.scheduleDesc}
                                 placeholder="Description"
@@ -45,26 +82,58 @@ const NewSchedule = ({ visible, hideModal }: any) => {
                                 mode="outlined"
                             />
                             {/* Schedule Time input */}
-                            <TextInput
-                                style={{marginTop: 10}}
-                                label="Schedule Time"
-                                value={formikProps.values.scheduleTime}
-                                placeholder="Schedule Time"
-                                onChangeText={formikProps.handleChange('scheduleTime')}
-                                autoComplete
-                                mode="outlined"
-                            />
+                            <View style={{
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                                marginTop: 10,
+                            }}>
+                                <TextInput
+                                    style={{
+                                        backgroundColor: 'white',
+                                        marginRight: 10,
+                                        width: '85%'
+                                    }}
+                                    label="Schedule Time"
+                                    value={formikProps.values.scheduleTime}
+                                    placeholder="Schedule Time"
+                                    onChangeText={formikProps.handleChange('scheduleTime')}
+                                    autoComplete
+                                    mode="outlined"
+                                    disabled
+                                />
+                                <TouchableOpacity
+                                    onPress={showTimepicker}
+                                >
+                                    <AntDesign name="clockcircleo" size={24} color="black" />
+                                </TouchableOpacity>
+                            </View>
                             {/* Schedule Date input */}
-                            <TextInput
-                                style={{marginTop: 10}}
-                                label="Schedule Date"
-                                value={formikProps.values.scheduleDate}
-                                placeholder="Schedule Date"
-                                onChangeText={formikProps.handleChange('scheduleDate')}
-                                autoComplete
-                                mode="outlined"
-                            />
-                            <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end'}}>
+                            <View style={{
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                                marginTop: 10,
+                            }}>
+                                <TextInput
+                                    style={{
+                                        backgroundColor: 'white',
+                                        marginRight: 10,
+                                        width: '85%'
+                                    }}
+                                    label="Schedule Date"
+                                    value={formikProps.values.scheduleDate}
+                                    placeholder="Schedule Date"
+                                    onChangeText={formikProps.handleChange('scheduleDate')}
+                                    autoComplete
+                                    mode="outlined"
+                                    disabled
+                                />
+                                <TouchableOpacity
+                                    onPress={showDatepicker}
+                                >
+                                    <MaterialIcons name="date-range" size={26} color="black" />
+                                </TouchableOpacity>
+                            </View>
+                            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end' }}>
                                 <Button
                                     color={Colors.red600}
                                     mode="contained"
@@ -77,7 +146,7 @@ const NewSchedule = ({ visible, hideModal }: any) => {
                                     color={Colors.blue600}
                                     mode="contained"
                                     onPress={formikProps.handleSubmit}
-                                    style={{ margin: 10, marginTop: 15, marginBottom: 0 }}
+                                    style={{ margin: 10, marginTop: 15, marginBottom: 0, marginRight: 0 }}
                                 >
                                     Add
                                 </Button>
@@ -85,6 +154,16 @@ const NewSchedule = ({ visible, hideModal }: any) => {
                         </View>
                     )}
                 </Formik>
+                {show && (
+                    <DateTimePicker
+                        testID="dateTimePicker"
+                        value={date}
+                        mode={mode}
+                        is24Hour={false}
+                        display="default"
+                        onChange={onChange}
+                    />
+                )}
             </Modal>
         </Portal>
     )
